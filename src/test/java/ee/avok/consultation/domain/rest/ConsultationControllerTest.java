7ba3436bd27ec85611f917c520d78100b8be9997
TKasekamp <toniskasekamp@gmail.com>
2016-09-30 12:05:01 +0300

    Test profile should be test
    Forgot that

--- src/test/java/ee/avok/consultation/domain/rest/ConsultationControllerTest.java
+++ src/test/java/ee/avok/consultation/domain/rest/ConsultationControllerTest.java
@@ -12,6 +12,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,6 +23,7 @@ import ee.avok.consultation.ConsultationWebApplication;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ConsultationWebApplication.class)
 @WebAppConfiguration
+@ActiveProfiles("test")
 public class ConsultationControllerTest {
 
 	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
