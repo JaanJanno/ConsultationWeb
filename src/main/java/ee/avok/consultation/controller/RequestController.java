51c595ed2b1ecfc321beaf52d16663d9ce43edbd
Salman Lashkarara <salmanlashkarara@gmail.com>
2016-10-06 23:57:57 +0300

    Load all requests is added #33

--- src/main/java/ee/avok/consultation/controller/RequestController.java
+++ src/main/java/ee/avok/consultation/controller/RequestController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -34,6 +35,18 @@ public class RequestController {
 		model.addAttribute("consultation", new ConsultationRequest());
 	
 		conServ.createConsultation(conReq);
-		
 	}
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/requests/detail/{id}")
+	public String ConsultationRequestDetail(@PathVariable int id,
+											@ModelAttribute ConsultationRequest conReq,
+											Model model) {
+		LOG.info("Consultation request with id {} is sent to show the detail", id);
+		model.addAttribute("consultation",conServ.findOne(id) );
+		return "detail";
+	}
+	
+	
+	
+	
 }
--- src/main/java/ee/avok/consultation/rest/ConsultationController.java
+++ src/main/java/ee/avok/consultation/rest/ConsultationController.java
@@ -64,11 +64,7 @@ public class ConsultationController {
 	}
 	
 	
-	
-	
-	
-	
-	
+
 	
 	
 }
