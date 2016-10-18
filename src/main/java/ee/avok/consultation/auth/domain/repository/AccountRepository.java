3b4d2d9978838b7acddb51ce0ae503290548b9b6
Salman Lashkarara <salmanlashkarara@gmail.com>
2016-10-18 12:04:34 +0300

    Repo is fixed #49

--- src/main/java/ee/avok/consultation/auth/domain/repository/AccountRepository.java
+++ src/main/java/ee/avok/consultation/auth/domain/repository/AccountRepository.java
@@ -1,10 +1,15 @@
 package ee.avok.consultation.auth.domain.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import ee.avok.consultation.auth.domain.model.Account;
+import ee.avok.consultation.auth.domain.model.Role;
 
-public interface AccountRepository extends CrudRepository<Account, Integer> {
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+	public List<Account> findByRole(Role role);
 
 	Account findById(int id);
 	
