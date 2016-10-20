48a7da6c1a6b4b61657360bcc7bbcb9a5d6c20c5
Salman Lashkarara <salmanlashkarara@gmail.com>
2016-10-20 11:59:16 +0300

    username is added to the  menu #56

--- src/main/java/ee/avok/consultation/controller/AccountController.java
+++ src/main/java/ee/avok/consultation/controller/AccountController.java
@@ -1,5 +1,7 @@
 package ee.avok.consultation.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +13,8 @@ import ee.avok.consultation.auth.domain.model.Account;
 import ee.avok.consultation.auth.domain.model.Role;
 import ee.avok.consultation.auth.domain.model.UnauthorizedException;
 import ee.avok.consultation.auth.service.AuthService;
+import ee.avok.consultation.domain.model.ConsultationRequest;
+import ee.avok.consultation.domain.model.ConsultationStatus;
 import ee.avok.consultation.dto.AccountDTO;
 import ee.avok.consultation.service.AccountService;
 
@@ -27,8 +31,11 @@ public class AccountController {
 	public String ManagingAccounts(@CookieValue(value = "session", defaultValue = "none") String session, Model model)
 			throws UnauthorizedException {
 		Account user = authServ.authenticateRequestForRole(session, Role.CONSULTANT);
+		model.addAttribute("username", user.getUsername());
+		model.addAttribute("name", user.getName());
 		model.addAttribute("user", new AccountDTO(user.getId()));
 		return "admin/manage_account";
+		
 	}
 
 	@RequestMapping(path = "/accounts/manage")
