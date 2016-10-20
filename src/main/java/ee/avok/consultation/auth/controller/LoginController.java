e00d6c61017ae9527f2cba871cf6b0579e5b7821
Salman Lashkarara <salmanlashkarara@gmail.com>
2016-10-20 12:38:21 +0300

    logout style is fixed #42

--- src/main/java/ee/avok/consultation/auth/controller/LoginController.java
+++ src/main/java/ee/avok/consultation/auth/controller/LoginController.java
@@ -42,12 +42,22 @@ public class LoginController {
 			return "redirect:" + "/login";
 		}
 	}
-	
+	/*
 	@RequestMapping(value = "/logout", method = RequestMethod.POST)
 	public String postLogout(@CookieValue(value = "session", defaultValue = "none") String session) {
 		authServ.endSession(session);
 		return "redirect:" + "/login";
 	}
+	*/
+	@RequestMapping(value = "/logout")
+	public String postLogout(@CookieValue(value = "session", defaultValue = "none") String session) {
+		authServ.endSession(session);
+		return "redirect:" + "/";
+	}
+	
+	
+	
+	
 
 	@ExceptionHandler(UnauthorizedException.class)
 	public String handleNotFound(Exception exc) {
