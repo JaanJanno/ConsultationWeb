9d5759b9db0d2148062898fd8abcf808eeb2d729
TKasekamp <toniskasekamp@gmail.com>
2016-12-04 16:56:12 +0200

    Sample data only in test profile. Close #134

--- src/main/java/ee/avok/consultation/DatabasePopulator.java
+++ src/main/java/ee/avok/consultation/DatabasePopulator.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
@@ -13,6 +14,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
+@Profile("test")
 public class DatabasePopulator {
 
 	@Autowired
